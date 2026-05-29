be536b09d2686a4034285ed55a094098582d97f8
Renato Britto <renatobritto@protonmail.com>
2026-05-29 18:06:49 -0300

    types: add v31 optional arg of gettxspendingprevout

--- client/src/client_sync/v31/blockchain.rs
+++ client/src/client_sync/v31/blockchain.rs
@@ -32,3 +32,30 @@ macro_rules! impl_client_v31__get_mempool_feerate_diagram {
         }
     };
 }
+
+/// Implements Bitcoin Core JSON-RPC API method `gettxspendingprevout`.
+#[macro_export]
+macro_rules! impl_client_v31__get_tx_spending_prevout {
+    () => {
+        impl Client {
+            pub fn get_tx_spending_prevout(
+                &self,
+                outputs: &[bitcoin::OutPoint],
+                mempool_only: bool,
+                return_spending_tx: bool,
+            ) -> Result<GetTxSpendingPrevout> {
+                let json_outputs: Vec<_> = outputs.iter().map(|out| {
+                    serde_json::json!({
+                        "txid": out.txid.to_string(),
+                        "vout": out.vout,
+                    })
+                }).collect();
+                let options = serde_json::json!({
+                    "mempool_only": mempool_only,
+                    "return_spending_tx": return_spending_tx,
+                });
+                self.call("gettxspendingprevout", &[json_outputs.into(), options])
+            }
+        }
+    };
+}
