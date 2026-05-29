4f422b62c5c15af7b8f3ff5899e8cafd272d8479
Renato Britto <renatobritto@protonmail.com>
2026-05-28 21:53:14 -0300

    types: fix v31 control logging
    New fields kernel and privatebroadcast.

---
+++ types/src/v31/control.rs
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: CC0-1.0
+
+//! The JSON-RPC API for Bitcoin Core `v31` - control.
+//!
+//! Types for methods found under the `== Control ==` section of the API docs.
+
+use serde::{Deserialize, Serialize};
+
+/// Result of JSON-RPC method `logging`.
+///
+/// > logging ( `<include>` `<exclude>` )
+///
+/// > Gets and sets the logging configuration.
+#[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
+#[cfg_attr(feature = "serde-deny-unknown-fields", serde(deny_unknown_fields))]
+pub struct Logging {
+    pub addrman: bool,
+    pub bench: bool,
+    pub blockstorage: bool,
+    pub cmpctblock: bool,
+    pub coindb: bool,
+    pub estimatefee: bool,
+    pub http: bool,
+    pub i2p: bool,
+    pub ipc: bool,
+    pub kernel: bool,
+    pub leveldb: bool,
+    pub libevent: bool,
+    pub mempool: bool,
+    pub mempoolrej: bool,
+    pub net: bool,
+    pub privatebroadcast: bool,
+    pub prune: bool,
+    pub proxy: bool,
+    pub qt: bool,
+    pub rand: bool,
+    pub reindex: bool,
+    pub rpc: bool,
+    pub scan: bool,
+    pub selectcoins: bool,
+    pub tor: bool,
+    pub txpackages: bool,
+    pub txreconciliation: bool,
+    pub validation: bool,
+    pub walletdb: bool,
+    pub zmq: bool,
+}
